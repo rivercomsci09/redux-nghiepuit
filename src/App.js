3b5d5166ee8ee8d58b3972b574310ba74fae462c
Tran Truong Giang <rivercomsci09@gmail.com>
2019-10-22 23:29:33 +0700

    - ls22

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import Control from './components/Control';
 import TaskList from './components/TaskList';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      tasks : [], //id, name, status
-      isDisplayForm : false
+      tasks: [], //id, name, status
+      isDisplayForm: false
     }
   }
 
   //Khi truy cap toi (F5: refresh), ham duoi se duoc tu dong goi va chi goi 1 lan
   //Dung de gan lai cai state tu localStorage 
-  componentWillMount(){
-    if(localStorage && localStorage.getItem('tasks')){
+  componentWillMount() {
+    if (localStorage && localStorage.getItem('tasks')) {
       var tasks = JSON.parse(localStorage.getItem('tasks'));
       this.setState({
-        tasks : tasks 
+        tasks: tasks
       })
     }
   }
 
-  s4(){
-    return Math.floor((1+Math.random()) * 0x10000).toString(16).substring(1);
+  s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
   }
 
-  generateID(){
+  generateID() {
     return this.s4() + this.s4() + '-' + this.s4();
   }
   onGenerateData = () => {
@@ -36,59 +36,82 @@ class App extends Component {
       {
         id: this.generateID(),
         name: 'Hoc lap trinh',
-        status: true       
+        status: true
       },
       {
         id: this.generateID(),
         name: 'Hoc lap trinh 02',
-        status: false       
+        status: false
       },
       {
         id: this.generateID(),
         name: 'Hoc lap trinh 03',
-        status: true       
+        status: true
       }
     ];
     this.setState({
-      tasks : tasks
+      tasks: tasks
     });
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 
   onToggleForm = () => {
     this.setState({
-      isDisplayForm : !this.state.isDisplayForm 
+      isDisplayForm: !this.state.isDisplayForm
     });
   }
 
   onCloseForm = () => {
     this.setState({
-      isDisplayForm : !this.state.isDisplayForm 
+      isDisplayForm: !this.state.isDisplayForm
     });
   }
 
   onSubmit = (data) => {
-    data.id  = this.generateID();
-    var {tasks} = this.state;
+    data.id = this.generateID();
+    var { tasks } = this.state;
     tasks.push(data);
     this.setState({
-      tasks : tasks
+      tasks: tasks
     });
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
+
+  onUpdateStatus = (id) => {
+    var {tasks} = this.state;
+    var index = this.findIndex(id);
+    if(index !== -1) {
+      tasks[index].status = !tasks[index].status;
+      this.setState({
+        tasks:tasks
+      });
+      localStorage.setItem('tasks',JSON.stringify(tasks));
+    }   
+  }
+
+  findIndex = (id) => {
+    var {tasks} = this.state;
+    var result = -1;
+    tasks.forEach((task,index) => {
+      if(task.id === id){
+        return result = index;
+      }
+    });
+    return result;
+  }
   render() {
-    var {tasks, isDisplayForm} = this.state; //var tasks = this.state.tasks 
+    var { tasks, isDisplayForm } = this.state; //var tasks = this.state.tasks 
     var elmTaskForm = isDisplayForm ? <TaskForm onSubmit={this.onSubmit} onCloseForm={this.onCloseForm} /> : '';
     return (
       <div className="container">
         <h1>Quản Lý Công Việc</h1>
         <div className="row">
-          <div className= {isDisplayForm ? 'col-xs-4 col-sm-4 col-md-4 col-lg-4' : ''}>
+          <div className={isDisplayForm ? 'col-xs-4 col-sm-4 col-md-4 col-lg-4' : ''}>
             {/*Form */}
             {elmTaskForm}
           </div>
           <div className={isDisplayForm ? 'col-xs-8 col-sm-8 col-md-8 col-lg-8' : 'col-xs-12 col-sm-12 col-md-12 col-lg-12'}>
-            <button type="button" className="btn btn-primary" onClick={ this.onToggleForm }>
+            <button type="button" className="btn btn-primary" onClick={this.onToggleForm}>
               <span className="fa fa-plus mr-5"></span>Thêm Công Việc
             </button>
             {/* <button type="button" className="btn btn-success ml-5" onClick={this.onGenerateData}>
@@ -99,7 +122,7 @@ class App extends Component {
             {/** List */}
             <div className="row mt-15">
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <TaskList tasks = { tasks }/>
+                <TaskList tasks={tasks} onUpdateStatus={this.onUpdateStatus} />
               </div>
             </div>
           </div>
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 class TaskItem extends Component {
+    onUpdateStatus = () => {
+        this.props.onUpdateStatus(this.props.task.id);
+    }
     render() {
-        var {task, index} = this.props;
+        var { task, index } = this.props;
         return (
             <tr>
                 <td> {index + 1} </td>
                 <td> {task.name} </td>
                 <td className="text-center">
-                    <span className= { task.status === true ? 'label label-success' : 'label label-danger' }> {task.status === true ? 'Active' : 'Hide'} </span>
+                    <span className={task.status === true ? 'label label-success' : 'label label-danger'} onClick={this.onUpdateStatus}> {task.status === true ? 'Active' : 'Hide'} </span>
                 </td>
                 <td className="text-center">
                     <button type="button" className="btn btn-warning">
